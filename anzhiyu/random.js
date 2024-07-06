93a7787bf37856002bad6905b17eb2b8feb0f9bb
nishuobianchenghennan <77688784@qq.com>
2024-07-06 13:12:00 +0800

    Site updated: 2024-07-06 13:11:56

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/30/hello-world/","2024/07/05/KSSOLV-3-0-structure/","2024/07/03/matlab中的面向对象编程/","2024/07/04/program-day2/","2024/07/03/程序编写日记/","2024/07/05/英语六级day1/"];function toRandomPost(){
+var posts=["2024/06/30/hello-world/","2024/07/05/KSSOLV-3-0-structure/","2024/07/04/program-day2/","2024/07/03/matlab中的面向对象编程/","2024/07/03/程序编写日记/","2024/07/05/英语六级day1/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
